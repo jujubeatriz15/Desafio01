d20b2736ebec4de5463084fa96644f3c6a3a9c26
Julia Beatriz <julia.beatriz206487@gmai.com>
2025-04-08 11:55:40 -0300

    desafio resolvido

--- exercicio01.py
+++ exercicio01.py
@@ -1,5 +1,19 @@
 #exercicio01
-horario01 = 3:45
-horario02 = 14:20
-horario01 = input("digite o primeiro horario: ")
-horario02 =
+h01 = int(input("Digite a Hora1" ))
+m01 = int(input("Digite a Hora2" ))
+h02 = int(input("Digite a Hora2" ))
+m02 = int(input("Digite a Hora2" ))
+
+somaH = h01 + m01
+somaH = h02 + m02
+
+if somaH>59:
+    somaH+=1
+    somaH = somaH - 60
+if somaH>=36:
+    somaH-=36
+elif somaH>=24:
+    somaH = somaH - 24
+elif somaH>=12:
+    somaH = somaH - 12
+print(somaH,somaH)
